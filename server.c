fb55aae52865af3edf72cf90ea9a74f820c4e6f0
Juan Esteban Pelaez Hoyos <jpelaez-@c2r6p8.hive.fi>
2023-01-30 17:34:01 +0200

    last commit 30/01/2022

--- server.c
+++ server.c
@@ -6,15 +6,27 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:16:33 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/01/30 15:24:58 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/01/30 17:33:23 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	signal_handler(int sig_num)
+{
+    
+}
+
 int	main(void)
 {
 	ft_printf("Bienevenido a mi Server");
 	ft_printf("My server PID is: %d", getpid());
-    
+
+	while (1)
+	{
+		signal(SIGUSR1, signal_handler);
+		signal(SIGUSR2, signal_handler);
+		pause();
+	}
+	return (0);
 }
