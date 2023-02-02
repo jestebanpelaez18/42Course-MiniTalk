2de26c044f2a302c2d9e11f55f10b78647077804
Juan Esteban Pelaez Hoyos <jpelaez-@c2r6p8.hive.fi>
2023-02-02 15:47:50 +0200

    last update 2/02/2022

--- server.c
+++ server.c
@@ -6,22 +6,34 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:16:33 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/01/30 17:33:23 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/02/02 15:45:02 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	signal_handler(int sig_num)
+void	signal_handler(int bit)
 {
-    
+	static int	ch;
+	static int	bit_counter;
+
+	if (bit == SIGUSR1)
+	{
+		ch = ch | (1 << bit_counter);
+	}
+	bit_counter++;
+	if (bit == 8)
+	{
+		printf("%c", ch);
+		ch = 0;
+		bit_counter = 0;
+	}
 }
 
 int	main(void)
 {
 	ft_printf("Bienevenido a mi Server");
 	ft_printf("My server PID is: %d", getpid());
-
 	while (1)
 	{
 		signal(SIGUSR1, signal_handler);
