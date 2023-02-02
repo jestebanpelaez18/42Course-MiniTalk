7d0b8e837c7f77544a6d0af3c537a95207bd1dc3
Juan Esteban Pelaez Hoyos <jpelaez-@c2r6p8.hive.fi>
2023-02-02 16:23:54 +0200

    last update 2/02/2023

--- client.c
+++ client.c
@@ -6,22 +6,26 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:16:40 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/01/30 15:40:18 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/02/02 16:23:22 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int bit_to_server()
+
+
 int	main(int argc, char **argv)
 {
-	int pid;
-	char *str;
+	int		pid;
+	char	*str;
 
 	if (argc == 3)
 	{
-		pid = argv[1];
+		pid = ft_atoi(argv[1]);
 		str = argv[2];
+		bit_to_server();
 	}
 	else
-		ft_printf("Just writ the PID and the message as arguments \n");
+		ft_printf("Just write the PID and the message as arguments \n");
 }
